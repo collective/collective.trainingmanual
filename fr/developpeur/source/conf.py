39aed0fb9f924934c047a6726060e3b0b9cdc42a
Vincent Fretin <vincent.fretin@gmail.com>
2010-12-31 08:45:59 +0000

    Enable sphinx.ext.intersphinx extension
    git-svn-id: https://svn.plone.org/svn/collective/collective.trainingmanual/trunk@229624 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- fr/developpeur/source/conf.py
+++ fr/developpeur/source/conf.py
@@ -22,7 +22,9 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.ifconfig']
+extensions = ['sphinx.ext.todo',
+              'sphinx.ext.ifconfig',
+              'sphinx.ext.intersphinx']
 
 todo_include_todos = False
 
