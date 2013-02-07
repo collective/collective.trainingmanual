06ba4e5a9548797759a4011d5f1119ba6471c939
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-02-07 13:14:41 +0100

    make it work with the eggified sphinx.plonetheme.

--- developpeur/source/conf.py
+++ developpeur/source/conf.py
@@ -22,9 +22,12 @@ import sys, os, time
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo',
-              'sphinx.ext.ifconfig',
-              'sphinx.ext.intersphinx']
+extensions = [
+    'sphinx.ext.todo',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.intersphinx',
+    'sphinxjp.themecore',
+]
 intersphinx_mapping = {'integrateur': ('http://docs.ecreall.com/integrateur', None)}
 
 todo_include_todos = False
@@ -102,16 +105,16 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-#html_theme = 'plonetheme'
+html_theme = 'plonetheme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 
 # Imitation du thème plone.org 2009
-#html_theme_options = {
-#    'disqus_name': 'plonepourlesintgrateurs'
-#    }
+html_theme_options = {
+    'disqus_name': 'plonepourlesintgrateurs'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = ['../..']
--- integrateur/source/conf.py
+++ integrateur/source/conf.py
@@ -22,9 +22,12 @@ import sys, os, time
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo',
-              'sphinx.ext.ifconfig',
-              'sphinx.ext.intersphinx']
+extensions = [
+    'sphinx.ext.todo',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.intersphinx',
+    'sphinxjp.themecore',
+]
 intersphinx_mapping = {'developpeur': ('http://docs.ecreall.com/developpeur', None)}
 
 todo_include_todos = False
@@ -96,15 +99,15 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-#html_theme = 'plonetheme'
+html_theme = 'plonetheme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 
-#html_theme_options = {
-#    'disqus_name': 'plonepourlesintgrateurs'
-#    }
+html_theme_options = {
+    'disqus_name': 'plonepourlesintgrateurs'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = ['../..']
